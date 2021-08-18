666f15b6258350bfde0fadb0630ba4fb3643ef1d
HaroldLee931 <jinbo.l931@gmail.com>
2021-08-18 20:44:34 +0800

    now can save text data to csv

--- back_end/app.py
+++ back_end/app.py
@@ -2,7 +2,12 @@ from flask import Flask, request, render_template, url_for
 from flask_cors import CORS
 import json
 import jieba
+import time
 # import jieba.posseg as pseg
+import pandas as pd
+
+# TODO: get file name list form folder
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -17,6 +22,19 @@ def getWordList(sentence:str):
   word_list = jieba.cut_for_search(sentence)
   for i in word_list:
     print(i)
+
+def saveTextToCSV(sentence:str):
+  audioTextData = {'timingString': [],
+                   'text':[]}
+  aTDSHandler = pd.DataFrame(audioTextData)
+  start = time.time()
+  ts = str(start)
+  newATData = {'timingString': ts, 'text': sentence}
+  aTDSHandler = aTDSHandler.append(newATData, ignore_index=True)
+  aTDSHandler.to_csv("tzzs_data2.csv",mode='a', index=None, header=False)
+  end = time.time()
+  app.logger.warning('Timing cost')
+  app.logger.warning(end - start)
 
 ###################################
 #   route to different web page   #
@@ -41,13 +59,21 @@ def visit_eggs_page():
 ###################################
 #     request data transfer       #
 ###################################
-@app.route("/data_pipeline", methods=['POST'])
-def saving_data():
-  # TODO: Saving audio data
+@app.route("/text_pipeline", methods=['POST'])
+def recive_text_data():
+
   # https://blog.csdn.net/zhangvalue/article/details/93884630
   # https://blog.csdn.net/longting_/article/details/80637002
-  text = json.loads(request.data.decode('utf8').replace("'", '"'))
-  app.logger.warning(text['usermsg'])  # same with <input name='usermsg'>
+  text = json.loads(request.data.decode('utf8').replace("'", '"')) # same with <input name='usermsg'>
+  saveTextToCSV(text['usermsg'])
+  return str("hhh")
+
+@app.route("/audio_pipeline", methods=['POST'])
+def recive_audio_data():
+  audio = request.data
+  # TODO: a_to_t_api(audio)
+  '''
+  '''
   return str("hhh")
 
 @app.route("/get_data", methods=['GET'])
@@ -63,5 +89,6 @@ def pop_data():
 if __name__ == '__main__':
   app.run(host='0.0.0.0',  # 任何ip都可以访问
           port=8988,  # 端口
-          debug=True
+          debug=True,
+          threaded=False
          )
