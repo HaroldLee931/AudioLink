0aaf28b635c9f2f2a4c19bdeb59f76c038a44dc8
Jinbo Li <jinbo.l931@gmail.com>
2021-08-19 01:22:07 +0800

    add file upload pipeline

--- back_end/app.py
+++ back_end/app.py
@@ -12,7 +12,7 @@ import pandas as pd
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 marker = 0
-jieba.enable_paddle()
+#jieba.enable_paddle()
 jieba.initialize()
 
 ###################################
@@ -23,29 +23,23 @@ def getWordList(sentence:str):
   for i in word_list:
     print(i)
 
-def saveTextToCSV(sentence:str):
+def saveTextToCSV(sentence:str, time_data:float):
   audioTextData = {'timingString': [],
                    'text':[]}
   aTDSHandler = pd.DataFrame(audioTextData)
-  start = time.time()
-  ts = str(start)
-  newATData = {'timingString': ts, 'text': sentence}
+  newATData = {'timingString': str(time), 'text': sentence}
   aTDSHandler = aTDSHandler.append(newATData, ignore_index=True)
-  aTDSHandler.to_csv("tzzs_data2.csv",mode='a', index=None, header=False)
+  aTDSHandler.to_csv("text_data.csv",mode='a', index=None, header=False)
   end = time.time()
   app.logger.warning('Timing cost')
-  app.logger.warning(end - start)
+  app.logger.warning(end - time_data)
 
 ###################################
 #   route to different web page   #
 ###################################
 @app.route("/")
-def hello_world():
-  return ("hello world")
-
-@app.route("/index")
 def visit_user_input_page():
-  return render_template('index.html')
+  return render_template('interface.html')
 
 @app.route("/visualization")
 def visit_visualization_page():
@@ -65,16 +59,20 @@ def recive_text_data():
   # https://blog.csdn.net/zhangvalue/article/details/93884630
   # https://blog.csdn.net/longting_/article/details/80637002
   text = json.loads(request.data.decode('utf8').replace("'", '"')) # same with <input name='usermsg'>
-  saveTextToCSV(text['usermsg'])
+  saveTextToCSV(text['usermsg'], time.time())
   return str("hhh")
 
 @app.route("/audio_pipeline", methods=['POST'])
 def recive_audio_data():
-  audio = request.data
+  audio = request.files['audio_file']
+  # for test only
+  audio.save(audio.filename)
+  '''
+  start = time.time()
+  audio.save(str(start))
   # TODO: a_to_t_api(audio)
   '''
-  '''
-  return str("hhh")
+  return str("haoyeah")
 
 @app.route("/get_data", methods=['GET'])
 def pop_data():
